28ea09a58a411d6c13cea962ce33935ca0bae948
Patoooo <sy2470@nyu.edu>
2022-11-22 23:16:11 +0800

    :earth_asia:  统一时间了

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -21,12 +21,24 @@ namespace TryScripts
         // Start is called before the first frame update
         void Start()
         {
-            //var curtime = (System.DateTime.Now - System.TimeZone.CurrentTimeZone.GetUtcOffset(System.DateTime.Now));
-            var curtime = System.DateTime.Now;
-            curTimeSecond = curtime.Second;
-            curTimeMinute = curtime.Minute;
-            curTimeHour = curtime.Hour;
-            curTimeString = curtime.ToString();
+            // var curtime = (System.DateTime.Now - System.TimeZone.CurrentTimeZone.GetUtcOffset(System.DateTime.Now));
+            //  var curtime;
+
+             if (VRC.SDKBase.Networking.IsNetworkSettled)
+             {
+                 var curtime = VRC.SDKBase.Networking.GetNetworkDateTime();
+                 curTimeSecond = curtime.Second;
+                 curTimeMinute = curtime.Minute;
+                 curTimeHour = curtime.Hour;
+                 curTimeString = curtime.ToString();
+             }
+            
+             else
+             {
+                 curTimeString = "fjsiodgwsd";
+             }
+
+            // curTimeString = VRC.SDKBase.Networking.IsNetworkSettled.ToString();
         }
 
         // Update is called once per frame
@@ -34,7 +46,7 @@ namespace TryScripts
         {
             curTimeInUI.text = curTimeString;
             //var curtime = (System.DateTime.Now - System.TimeZone.CurrentTimeZone.GetUtcOffset(System.DateTime.Now));
-            var curtime = System.DateTime.Now;
+            var curtime = VRC.SDKBase.Networking.GetNetworkDateTime();
             curTimeSecond = curtime.Second;
             curTimeMinute = curtime.Minute;
             curTimeHour = curtime.Hour;
