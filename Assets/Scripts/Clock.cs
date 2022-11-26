fb89f2e5d231e100114eaadbd80a89cd3672e5ee
Joyce Z <yz4867@nyu.edu>
2022-11-26 21:25:51 +0800

    我是Pato 加好了虚拟时间 准备做昼夜系统

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -13,10 +13,15 @@ namespace TryScripts
     {
 
         public Text curTimeInUI;
+        public int timeRatio = 12;
         [SerializeField] public int curTimeSecond;
         [SerializeField] public int curTimeMinute;
         [SerializeField] public int curTimeHour;
         [SerializeField] public string curTimeString;
+        [SerializeField] public string curGameVirtualTimeString;
+
+        [SerializeField] public int localTimeHour, localTimeMinute, localTimeSecond;
+
 
         // Start is called before the first frame update
         void Start()
@@ -24,19 +29,19 @@ namespace TryScripts
             // var curtime = (System.DateTime.Now - System.TimeZone.CurrentTimeZone.GetUtcOffset(System.DateTime.Now));
             //  var curtime;
 
-             if (VRC.SDKBase.Networking.IsNetworkSettled)
-             {
-                 var curtime = VRC.SDKBase.Networking.GetNetworkDateTime();
-                 curTimeSecond = curtime.Second;
-                 curTimeMinute = curtime.Minute;
-                 curTimeHour = curtime.Hour;
-                 curTimeString = curtime.ToString();
-             }
-            
-             else
-             {
-                 curTimeString = "fjsiodgwsd";
-             }
+            if (VRC.SDKBase.Networking.IsNetworkSettled)
+            {
+                var curtime = VRC.SDKBase.Networking.GetNetworkDateTime();
+                curTimeSecond = curtime.Second;
+                curTimeMinute = curtime.Minute;
+                curTimeHour = curtime.Hour;
+                curTimeString = curtime.ToString();
+            }
+
+            else
+            {
+                curTimeString = "fjsiodgwsd";
+            }
 
             // curTimeString = VRC.SDKBase.Networking.IsNetworkSettled.ToString();
         }
@@ -44,13 +49,58 @@ namespace TryScripts
         // Update is called once per frame
         void Update()
         {
-            curTimeInUI.text = curTimeString;
+            curTimeInUI.text = "The Real Current Time Now is " + curTimeString + "\n" + "The Virtual Time is " + curGameVirtualTimeString;
             //var curtime = (System.DateTime.Now - System.TimeZone.CurrentTimeZone.GetUtcOffset(System.DateTime.Now));
             var curtime = VRC.SDKBase.Networking.GetNetworkDateTime();
             curTimeSecond = curtime.Second;
             curTimeMinute = curtime.Minute;
             curTimeHour = curtime.Hour;
             curTimeString = curtime.ToString();
+
+            GameVirtualTime();
+        }
+
+
+        void GameVirtualTime()
+        {
+            int localTotalTime = (curTimeHour % 2) * 60 * 60 + (curTimeMinute) * 60 + curTimeSecond;
+            localTimeHour = localTotalTime / (3600 / timeRatio);
+            int localTotalTimeRemainAfterHour = localTotalTime % (3600 / timeRatio);
+            localTimeMinute = localTotalTimeRemainAfterHour / (60 / timeRatio);
+            int localTotalTimeRemainAfterMinute = localTotalTimeRemainAfterHour % (60 / timeRatio);
+            localTimeSecond = localTotalTimeRemainAfterMinute * timeRatio;
+
+            string localTimeHourString, localTimeMinuteString, localTimeSecondString;
+
+            if (localTimeHour < 10)
+            {
+                localTimeHourString = "0" + localTimeHour.ToString();
+            }
+            else
+            {
+                localTimeHourString = localTimeHour.ToString();
+            }
+
+            if(localTimeMinute < 10)
+            {
+                localTimeMinuteString = "0" + localTimeMinute.ToString();
+            }
+            else
+            {
+                localTimeMinuteString = localTimeMinute.ToString();
+            }
+
+            if (localTimeSecond < 10)
+            {
+                localTimeSecondString = "0" + localTimeSecond.ToString();
+            }
+            else
+            {
+                localTimeSecondString = localTimeSecond.ToString();
+            }
+
+            curGameVirtualTimeString = localTimeHourString + " : " + localTimeMinuteString + " : " + localTimeSecondString;
         }
     }
+
 }
---
+++ Assets/Scripts/DayNightEventSystem.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DayNightEventSystem : UdonSharpBehaviour
+{
+    public int dayHourTime;
+    public int nightHourTime;
+
+    public int virtualTimeHour;
+
+    void Start()
+    {
+        
+    }
+
+    void update()
+    {
+       if(virtualTimeHour > dayHourTime)
+        {
+
+        }
+    }
+}
