8e854b9cbb9ad9ef49024ce1896d9ba9354f83c9
Joyce Z <yz4867@nyu.edu>
2022-11-28 23:51:46 +0800

    【大跪拜】daynightevent出现一些bug dunno why 场景新增 有事随时微信回复……

--- Assets/Scripts/DayNightEventSystem.cs
+++ Assets/Scripts/DayNightEventSystem.cs
@@ -10,7 +10,7 @@ namespace TryScripts
     public class DayNightEventSystem : UdonSharpBehaviour
     {
         public int virtualTimeHour;
-        
+
         [Header("和昼夜相关")]
         public int dayHourTime;
         public int nightHourTime;
@@ -19,30 +19,42 @@ namespace TryScripts
         public Material nightSkybox;
         public UdonBehaviour clockUdon;
 
-        [Header("和拍卖相关")] 
+        [Header("和拍卖相关")]
         public int firstAuctionTime;
         public int secondAuctionTime;
-        
+
         public UdonBehaviour auctionUdon;
         public bool canAuction;
 
-       public UdonSharpBehaviour[] doors;
-       public GameObject door1;
-       Collider _door1Collider;
-       
-        
-        
+        //public UdonSharpBehaviour[] doors;
+        public GameObject door1;
+        Collider _door1Collider;
+
+        public GameObject _btn1;
+        public GameObject _btn2;
+        public GameObject _btn3;
+        public GameObject _btn4;
+
+
+
         // public Clock clockUdon;
         void Start()
         {
             canAuction = true;
             // virtualTimeHour = clockUdon.localTimeHour;
-           
 
-           //❗️不知道怎么udon findgameobjectswithtag
-            // doors = GameObject.FindGameObjectsWithTag("rm_door"); 
 
-             _door1Collider = door1.GetComponent<Collider>();
+            //❗️不知道怎么udon findgameobjectswithtag
+            // doors = GameObject.FindGameObjectsWithTag("rm_door");
+
+            _door1Collider = door1.GetComponent<Collider>();
+
+            _btn1.SetActive(false);
+            _btn2.SetActive(false);
+            _btn3.SetActive(false);
+            _btn4.SetActive(false);
+
+
         }
 
         void Update()
@@ -51,17 +63,25 @@ namespace TryScripts
             virtualTimeHour = (int)clockUdon.GetProgramVariable("localTimeHour");
 
             // in the Day
-            if (virtualTimeHour >= dayHourTime && virtualTimeHour < nightHourTime)
+            if (virtualTimeHour > dayHourTime && virtualTimeHour < nightHourTime)
             {
                 RenderSettings.skybox = daySkybox;
                 _door1Collider.enabled = true;
 
-                if (virtualTimeHour > firstAuctionTime)
+                if (virtualTimeHour > firstAuctionTime && virtualTimeHour < secondAuctionTime)
                 {
                     DoAuction();
+
+                    ButtonActivated();
+
+                }
+                else
+                {
+                    ButtonDeActivated();
                 }
 
-                //Activate Buttons
+
+
             }
 
             // in the Night
@@ -70,8 +90,9 @@ namespace TryScripts
                 RenderSettings.skybox = nightSkybox;
 
                 //门collider消失
-               
                 _door1Collider.enabled = false;
+
+                ButtonDeActivated();
             }
 
         }
@@ -87,6 +108,22 @@ namespace TryScripts
             }
         }
 
-    
+        void ButtonActivated()
+        {
+            _btn1.SetActive(true);
+            _btn2.SetActive(true);
+            _btn3.SetActive(true);
+            _btn4.SetActive(true);
+        }
+
+        void ButtonDeActivated()
+        {
+            _btn1.SetActive(false);
+            _btn2.SetActive(false);
+            _btn3.SetActive(false);
+            _btn4.SetActive(false);
+        }
+
+
     }
 }
