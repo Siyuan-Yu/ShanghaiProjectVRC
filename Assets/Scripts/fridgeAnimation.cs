f3a64d8b0bf3aeec2c9759f4d1812593dc5080ee
Joyce Z <yz4867@nyu.edu>
2022-12-05 20:06:58 +0800

    can put things into the fridge now

---
+++ Assets/Scripts/fridgeAnimation.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class fridgeAnimation : UdonSharpBehaviour
+{
+    public string doorMove;
+
+    public override void Interact()
+    {
+       
+    }
+    void Start()
+    {
+        
+    }
+}
