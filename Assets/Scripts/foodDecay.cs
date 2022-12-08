a3062d2a24f814fb8122f6129a22593d37cdbdae
Joyce Z <yz4867@nyu.edu>
2022-12-08 18:07:15 +0800

    正方形变透明了

---
+++ Assets/Scripts/foodDecay.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class foodDecay : UdonSharpBehaviour
+{
+
+    //NEED TO REPLACE THIS TIME TO THE SYSTEM TIME
+    public float startTime;
+    public float fadeTime;
+
+    public GameObject food1;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        //IF IT COLLIDES WITH FRIDGE THEN NOTHING HAPPENS
+
+        //ELSE IF
+        float transparency = 1.0f- ( Time.time - startTime ) / fadeTime;
+        transparency = Mathf.Clamp01(transparency);
+
+        Debug.Log(transparency);
+
+        Material material = food1.GetComponent<Renderer>().material;
+
+        material.color = new Color(material.color.r, material.color.g, material.color.b, transparency);
+    }
+}
