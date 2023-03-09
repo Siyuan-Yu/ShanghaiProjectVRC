b9e078306a3c425a39fe995cf16d764bdcb12f11
Patoooo <sy2470@nyu.edu>
2023-03-10 00:28:03 +0800

    :+1:  item can be held by player, but trigger need to be checked

--- Assets/Scripts/CollideAddPoints.cs
+++ Assets/Scripts/CollideAddPoints.cs
@@ -1,29 +1,33 @@
-﻿
-using System;
+﻿using TryScripts;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
 
 public class CollideAddPoints : UdonSharpBehaviour
 {
     public UdonBehaviour pointSystem;
     public int localPoint;
     public int detectLayer;
+    
     void Start()
     {
         // pointSystem = PointSystem
     }
 
-    public override void OnPlayerTriggerEnter(VRCPlayerApi col)
-    {
-        if (col == Networking.LocalPlayer)
-        {
-            localPoint = (int)pointSystem.GetProgramVariable("points");
-            localPoint += 2;
-            pointSystem.SetProgramVariable("points", localPoint);
-        }
-    }
+    // public override void OnPlayerTriggerEnter(VRCPlayerApi col)
+    // {
+    //     // if (col == Networking.LocalPlayer)
+    //     // {
+    //         localPoint = (int)pointSystem.GetProgramVariable("points");
+    //         localPoint += 2;
+    //         pointSystem.SetProgramVariable("points", localPoint);
+    //     // }
+    // }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -36,4 +40,5 @@ public class CollideAddPoints : UdonSharpBehaviour
             Destroy(other.gameObject);
         }
     }
+    
 }
---
+++ Assets/Scripts/ItemBelongTest.cs
@@ -0,0 +1,34 @@
+﻿using TryScripts;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class ItemBelongTest : UdonSharpBehaviour
+{
+    public int playerID;
+
+    public Text idText;
+    void Start()
+    {
+        playerID = 100;
+    }
+    
+    public override void OnPlayerTriggerEnter(VRCPlayerApi other)
+    {
+        VRCPlayerApi player = other; 
+        // Check if the player exists and is active
+        if (player != null)
+        {
+            playerID = VRCPlayerApi.GetPlayerId(player);
+        }
+    }
+
+    private void Update()
+    {
+        idText.text = playerID.ToString();
+    }
+}
