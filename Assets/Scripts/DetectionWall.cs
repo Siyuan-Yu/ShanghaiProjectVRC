893900d89fa7e6caa8df47e8397d2b52e49ced42
Patoooo <sy2470@nyu.edu>
2023-06-04 19:18:52 +0800

    可以扣分和强行出门了

---
+++ Assets/Scripts/DetectionWall.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DetectionWall : UdonSharpBehaviour
+{
+    public BoxCollider col;
+    public UdonBehaviour wallTrigger;
+
+
+    // public bool triggered;
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+        if ((bool)wallTrigger.GetProgramVariable("isTriggered") == true)
+        {
+            col.enabled = false;
+        }
+        else
+        {
+            col.enabled = true;
+        }
+        
+        
+
+        // SendCustomEventDelayedSeconds("ReCoverWall", 10f);
+    }
+    
+}
---
+++ Assets/Scripts/DetectionWallTrigger.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DetectionWallTrigger : UdonSharpBehaviour
+{
+    public UdonBehaviour pointSystem;
+    public bool isTriggered;
+    public int playerPoint;
+    public int pointLine;
+
+    public int storeSecond;
+    public int curSecond;
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        curSecond = VRC.SDKBase.Networking.GetNetworkDateTime().Second;
+        playerPoint = (int)pointSystem.GetProgramVariable("points");
+
+        if (isTriggered)
+        {
+            // if (storeSecond <= 30 && curSecond > 49)
+            // {
+            if (curSecond >= storeSecond)
+            {
+                isTriggered = false;
+            }
+            // }
+        }
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player != null)
+        {
+            if (playerPoint > pointLine)
+            {
+                
+                storeSecond = VRC.SDKBase.Networking.GetNetworkDateTime().Second;
+                if (storeSecond < 49)
+                {
+                    storeSecond += 10;
+                }
+                else
+                {
+                    storeSecond = 30;
+                }
+                
+                isTriggered = true;
+
+                playerPoint -= 8;
+                pointSystem.SetProgramVariable("points", playerPoint);
+            }
+        }
+    }
+}
