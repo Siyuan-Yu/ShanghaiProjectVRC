a9dd1c1ec9eced3b1470d12576ed32a57c1f0600
Patoooo <sy2470@nyu.edu>
2024-05-05 22:48:07 +0800

    :+1:  滚动的可乐

---
+++ Assets/Scripts/RollingCoke.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RollingCoke : UdonSharpBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        // 使罐子前进并且滚动
+        transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, 360 * speed * Time.deltaTime, Space.World);
+    }
+}
