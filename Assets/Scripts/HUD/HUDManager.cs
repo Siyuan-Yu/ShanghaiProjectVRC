0323b44375b1af67a47bc1bef0c56bf28a26764a
Billiton Peng <62238461+BillyPung@users.noreply.github.com>
2024-12-20 18:29:27 +0800

    See detail, include Odin and DotweenPro
    2. Realized first HUD and Surrounding object
    3. Have HUD manager. (further test and implementation needed)
    4. Earned points to show on HUD (test needed)
    5. day count (havent found the data)
    6. inventory system (how to be seen by other players as well?) (auto layout to be done)

---
+++ Assets/Scripts/HUD/HUDManager.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using Sirenix.OdinInspector;
+using TMPro;
+
+namespace HUD
+{
+    public class HUDManager : UdonSharpBehaviour
+    {
+        [Title("Points earned")] 
+        [Required]
+        public PointSystem pointSystem;
+        [ChildGameObjectsOnly,Required]
+        public TextMeshProUGUI earnedPoints;
+
+        [InfoBox("Use {0} in the string to show earned points and \\n for a new line.")]
+        public string earnedPointsFormat = "Earned: {0} points";
+
+        [Title("Day Count")] [Required, ChildGameObjectsOnly,InfoBox("TODO")]
+        public TextMeshProUGUI dayCount;
+
+        [Title("Inventory")] [Required, ChildGameObjectsOnly,InfoBox("TODO")]
+        public Transform inventory;
+        private void Update()
+        {
+            earnedPoints.text = string.Format(earnedPointsFormat, (int)pointSystem.GetProgramVariable("points")); //TODO: Need further test.
+        }
+    }
+
+}
---
+++ Assets/Scripts/HUD/PlayerFollower.cs
@@ -0,0 +1,44 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using Sirenix.OdinInspector;
+
+namespace HUD
+{
+    public class PlayerFollower : UdonSharpBehaviour
+    {
+        private VRCPlayerApi _localPlayer;
+
+        [SerializeField]
+        private bool useMotionSmoothing = true;
+
+        [SerializeField, Range(1, 20), ShowIf("useMotionSmoothing")]
+        private float motionSmoothing = 18f;
+
+        public void Start()
+        {
+            if (Networking.LocalPlayer.IsValid())
+            {
+                _localPlayer = Networking.LocalPlayer;
+            }
+        }
+
+        public override void PostLateUpdate()
+        {
+            var head = _localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+
+            transform.position = head.position;
+            if (useMotionSmoothing)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, head.rotation,
+                    1.0f - Mathf.Exp(-motionSmoothing * Time.deltaTime));
+            }
+            else
+            {
+                transform.rotation = head.rotation;
+            }
+        }
+    }
+}
