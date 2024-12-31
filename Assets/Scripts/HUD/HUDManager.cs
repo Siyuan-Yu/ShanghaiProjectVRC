8d3d56bff67f1398b49d01c9e828505f7f2438db
Billiton Peng <62238461+BillyPung@users.noreply.github.com>
2024-12-31 17:27:43 +0800

    Recreated HUD Manager and it works now.
     Use Jump(space, etc.) to toggle the HUD

--- Assets/Scripts/HUD/HUDManager.cs
+++ Assets/Scripts/HUD/HUDManager.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System.Drawing.Printing;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -12,57 +11,59 @@ namespace HUD
 {
     public class HUDManager : UdonSharpBehaviour
     {
-       // [Title("HUD Canvas", "Probably the child Canvas, would be disabled initially"), Required]//, ChildGameObjectsOnly]
-        //public GameObject hudCanvas;
-        //private bool _isHUDEnabled;
-        [Title("Points earned")] 
-        [Required]
-        public PointSystem pointSystem;
-        [ChildGameObjectsOnly,Required]
-        public TextMeshProUGUI earnedPoints;
+        [Title("HUD Canvas", "Probably the child Canvas, would be disabled initially"), Required, ChildGameObjectsOnly]
+        public GameObject hudCanvas;
+
+        private bool _isHUDEnabled;
+        [Title("Points earned")] [Required] public PointSystem pointSystem;
+        [ChildGameObjectsOnly, Required] public TextMeshProUGUI earnedPoints;
 
         [InfoBox("Use {0} in the string to show earned points and \\n for a new line.")]
         public string earnedPointsFormat = "Earned: {0} points";
 
-        [Title("Day Count")] [Required, ChildGameObjectsOnly,InfoBox("TODO")]
+        [Title("Day Count")] [Required, ChildGameObjectsOnly, InfoBox("TODO")]
         public TextMeshProUGUI dayCount;
 
-        [Title("Inventory")] [Required, ChildGameObjectsOnly]//,InfoBox("TODO")]
+        [Title("Inventory")] [Required, ChildGameObjectsOnly] //,InfoBox("TODO")]
         public Transform inventory;
 
-        // private void Start()
-        // {
-        //     //if(!hudCanvas) hudCanvas = transform.Find("HUD Canvas").gameObject;
-        //     if (hudCanvas) 
-        //         hudCanvas.SetActive(false);
-        //     else
-        //     {
-        //         
-        //         Debug.LogWarning("The HUD Canvas is not assigned on HUD MANAGER!");
-        //     }
-        // }
+        [Title("Setting")] [SerializeField] private bool debugMode = false;
+
+        private void Start()
+        {
+            if (!hudCanvas) hudCanvas = transform.Find("HUD Canvas").gameObject;
+            if (hudCanvas)
+                hudCanvas.SetActive(false);
+            else
+            {
+                Debug.LogWarning("The HUD Canvas is not assigned on HUD MANAGER!");
+            }
+        }
 
         private void Update()
         {
-            if(earnedPoints)
-                earnedPoints.text = string.Format(earnedPointsFormat, (int)pointSystem.GetProgramVariable("points")); //TODO: Need further test.
-            Debug.Log("points1: " + (int)pointSystem.GetProgramVariable("points") + $"\n points2: {pointSystem.points}");
-            // if (Input.GetKeyDown(KeyCode.H))
-            // {
-            //     hudCanvas.SetActive(!hudCanvas.activeSelf);
-            // }
+            if (earnedPoints)
+                earnedPoints.text =
+                    string.Format(earnedPointsFormat,
+                        (int)pointSystem.GetProgramVariable("points")); //TODO: Need further test.
+            if (debugMode)
+                Debug.Log("points1: " + (int)pointSystem.GetProgramVariable("points") +
+                          $"\n points2: {pointSystem.points}");
+            /*if (Input.GetKeyDown(KeyCode.H))
+            {
+                hudCanvas.SetActive(!hudCanvas.activeSelf);
+            }*/
         }
-        
-        // public override void InputJump(bool value, UdonInputEventArgs args)
-        // {//Override jump to show or hide the hud and inventory
-        //     // This uses the "Jump" input (A button or Space key by default)
-        //     if (value && hudCanvas) // Button pressed
-        //     {
-        //         _isHUDEnabled = !_isHUDEnabled;
-        //         hudCanvas.SetActive(_isHUDEnabled);
-        //         
-        //     }
-        // }
-    }
 
+        public override void InputJump(bool value, UdonInputEventArgs args)
+        {
+            //Override jump to show or hide the hud and inventory
+            //  This uses the "Jump" input (A button or Space key by default)
+            if (value && hudCanvas) //Button pressed
+            {
+                //_isHUDEnabled = !_isHUDEnabled;
+                hudCanvas.SetActive(!hudCanvas.activeSelf);
+            }
+        }
+    }
 }
